d4196e809f94485ff53540730b1bd406e1d58358
vkumar825 <vkumar01825@gmail.com>
2022-12-24 13:28:44 -0500

    rewrote contact book methods to use list of dict

--- src/contact_book.py
+++ src/contact_book.py
@@ -1,86 +1,65 @@
 # for unit testing
-# from src.trie import Trie
+from src.trie import Trie
 
 # for tkinter app
-from trie import Trie
+# from trie import Trie
 
 
 class ContactBook():
 
     def __init__(self):
+
+        # TODO: Use a list of dictionary instead of nested dict for mongodb
         self.trie = Trie()
-        self.contacts = {}
-        self.search_result = {}
+        self.contacts = []
+        self.search_result = []
 
     def add_contact(self, name, phone_number):
 
         if (len(self.contacts) > 0):
-            for contact in self.contacts.values():
-                if contact["phone_number"] == phone_number:
-                    existing_contact = contact["name"]
+            for i in range(len(self.contacts)):
+                if self.contacts[i].get("phone_number") == phone_number:
+                    existing_contact = self.contacts[i].get("name")
                     raise Exception(
                         f"Failed to add contact. {phone_number} already used for contact {existing_contact}")
 
         if (name not in self.trie.query("")):
             self.trie.insert(name)
-            self.contacts[len(self.trie.query("")) - 1] = {"name": name,
-                                                           "phone_number": phone_number}
+            self.contacts.append({"name": name,
+                                  "phone_number": phone_number})
+            self.contacts.sort(key=lambda x: x["name"])
 
         else:
             raise Exception(f"Failed to add contact. {name} already exists.")
 
     def remove_contact(self, name):
 
-        temp_dict = {}
+        if (len(self.trie.query(name)) == 0):
+            raise Exception("Cannot remove non-existent name.")
 
-        for key in self.contacts.keys():
-            if self.contacts[key]["name"] == name:
-                key_to_remove = key
-
-        self.trie.delete(name)
-
-        for key, value in self.contacts.items():
-            if key < key_to_remove:
-                temp_dict[key] = value
-            elif key > key_to_remove:
-                temp_dict[key-1] = value
-            else:
-                continue
-
-        self.contacts = temp_dict
-
-    def get_ordered_contacts(self):
-
-        temp_dict = {}
-
-        # getting the keys from nested dict in the alphabetical order of names
-        sorted_keys = [key for key in dict(
-            sorted(self.contacts.items(), key=lambda x: x[1]["name"]))]
-
-        for i in range(len(self.trie.query(""))):
-            temp_dict[i] = {"name": self.contacts.get(sorted_keys[i]).get("name"),
-                            "phone_number": self.contacts.get(sorted_keys[i]).get("phone_number")}
-
-        self.contacts = temp_dict
+        list_of_names = [d["name"] for d in self.contacts]
+        for i in range(len(list_of_names)):
+            if list_of_names[i] == name:
+                self.contacts.pop(i)
+                self.trie.delete(name)
 
     def get_contacts_by_search(self, search_input):
 
         if (len(search_input) == 0):
-            self.get_ordered_contacts()
+            self.search_result = self.contacts
 
-        search_dict = {}
+        results = []
         list_of_names = self.trie.query(search_input)
 
         if (len(list_of_names) > 0):
             for i in range(len(list_of_names)):
-                for contact in self.contacts.values():
-                    if list_of_names[i] == contact["name"]:
-                        search_dict[i] = {"name": contact["name"],
-                                          "phone_number": contact["phone_number"]}
-            self.search_result = search_dict
-
+                for j in range(len(self.contacts)):
+                    if list_of_names[i] == self.contacts[j].get("name"):
+                        results.append({"name": self.contacts[j].get("name"),
+                                        "phone_number": self.contacts[j].get("phone_number")})
+            self.search_result = results
         else:
-            self.search_result = search_dict
+            self.search_result = results
 
     def get_trie(self):
         return self.trie
